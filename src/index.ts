92be973965331511acd73630529c40385bf7cdf3
Erodenn <90123623+Erodenn@users.noreply.github.com>
2026-07-11 18:07:18 -0400

    chore: release v3.2.1

--- src/index.ts
+++ src/index.ts
@@ -94,7 +94,7 @@ class GodotMcpServer {
     this.server = new Server(
       {
         name: 'godot-mcp',
-        version: '3.2.0',
+        version: '3.2.1',
       },
       {
         capabilities: {
